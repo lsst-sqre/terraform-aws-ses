c90a4c23a9a8b19025d33a1145793fb388a3f412
Joshua Hoblitt <josh@hoblitt.com>
2019-02-08 11:15:06 -0700

    declare version constraints for providers used by mods

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+provider "archive" {
+  version = "~> 1.1"
+}
+
+provider "random" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}
+
 provider "aws" {
   version = "~> 1.21"
   region  = "${var.aws_region}"
